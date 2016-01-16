8f2f1c5e4c21e36208cb44f7e38f394bc7d47829
motoki-shun <motoki-shun@kayac.com>
2016-01-16 16:51:17 +0900

    Tetris.js更新＆いろいろ調整

--- src/js/Tetris.js
+++ src/js/Tetris.js
@@ -1,6 +1,8 @@
 (function(win, doc){
   var ns = win.App = win.App || {};
   
+  var util = new ns.Util();
+  
   ns.Tetris = function(){
     ns.EventDispatcher.call(this);
     
@@ -8,8 +10,10 @@
     
     this.COLS = 15;
     this.ROWS = 30;
-    this.BLOCK_SIZE = 20;
+    this.BLOCK_SIZE = 14;
     this.NUMBER_OF_BLOCK = 5;
+    this.HIDDEN_ROWS = this.NUMBER_OF_BLOCK;
+    this.LOGICAL_ROWS = this.ROWS + this.HIDDEN_ROWS;
     this.WIDTH = this.BLOCK_SIZE * this.COLS;
     this.HEIGHT = this.BLOCK_SIZE * this.ROWS;
     this.NEXT_WIDTH = this.BLOCK_SIZE * this.NUMBER_OF_BLOCK;
@@ -19,6 +23,9 @@
     this.SPEEDUP_RATE = 10;
     this.START_X = Math.floor((this.COLS - this.NUMBER_OF_BLOCK) / 2);
     this.START_Y = 0; // -this.NUMBER_OF_BLOCK;
+    this.SPECIAL_SHAPE_INCIDENCE = 13;
+    this.CLEARLINE_BLOCK_INDEX = 14;
+    this.GAMEOVER_BLOCK_INDEX = 15;
     this.SHAPE_LIST = [
       [ 0, 0, 1, 0, 0,
         0, 0, 1, 0, 0,
@@ -56,6 +63,13 @@
         0, 0, 0, 0, 0,
         0, 0, 0, 0, 0 ],
     ];
+    this.SPECIAL_SHAPE_LIST = [
+      [ 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0 ],
+    ];
     this.COLOR_LIST = [
       "#FF6666",
       "#FFCC66",
@@ -69,40 +83,39 @@
       "#CC66FF",
       "#FF66FF",
       "#FF6FCF",
+      "#FF00FF",
+      "#FF8000",
+      "#4C4C4C",
     ];
     this.KEYS = {
       37: 'left',  // ←
       39: 'right',  // →
-      // 40: 'down',  // ↓
-      // 38: 'rotate',  // ↑
-      38: 'down',  // ↓ 上下反転
-      40: 'rotate',  // ↑ 上下反転
-      // 32: 'stop'  // space
+      40: 'down',  // ↓
+      38: 'rotate',  // ↑
       32: 'rotate'  // space
     };
-    
-    this.tickId;
-    this.lose;
-    this.board = [];
-    this.currentBlock = [];
-    this.currentX;
-    this.currentY;
-    this.numberOfClearLines;
-    
     
     this.$clearLine = $('#clearLine');
     this.$score = $('#score');
     this.$cnvs = $('#canvas');
+    this.$cnvsNext = $('#nextBlock');
     this.cnvs = document.getElementById('canvas');
     this.ctx = this.cnvs.getContext('2d');
     this.cnvsNext = document.getElementById('nextBlock');
     this.ctxNext = this.cnvsNext.getContext('2d');
     
-    this.cnvs.width = this.WIDTH;
-    this.cnvs.height = this.HEIGHT;
+    this.$cnvs.width(this.WIDTH);
+    this.$cnvs.height(this.HEIGHT);
+    this.cnvs.width = this.WIDTH * 2; // for retina
+    this.cnvs.height = this.HEIGHT * 2; // for retina
+    this.ctx.scale(2,2); // for retina
     this.ctx.strokeStyle = '#eee';
-    this.cnvsNext.width = this.NEXT_WIDTH;
-    this.cnvsNext.height = this.NEXT_HEIGHT;
+    
+    this.$cnvsNext.width(this.NEXT_WIDTH);
+    this.$cnvsNext.height(this.NEXT_HEIGHT);
+    this.cnvsNext.width = this.NEXT_WIDTH * 2; // for retina
+    this.cnvsNext.height = this.NEXT_HEIGHT * 2; // for retina
+    this.ctxNext.scale(2,2); // for retina
     this.ctxNext.strokeStyle = '#eee';
     
     this.setBlurEvent();
@@ -110,8 +123,6 @@
     this.setTouchEvent();
     
     this.renderId = setInterval(function(){ _this.render(); }, this.RENDER_INTERVAL);
-    
-    this.newGame();
   };
   ns.Tetris.prototype = Object.create(ns.EventDispatcher.prototype);
   ns.Tetris.prototype.constructor = ns.Tetris;
@@ -123,7 +134,7 @@
     $(window).on('blur',function(){
         _this.pauseGame();
     }).on('focus',function(){
-        _this.restartGame();
+        _this.resumeGame();
     });
   };
   
@@ -142,25 +153,36 @@
     var touch = new ns.TouchController(this.$cnvs);
     var touchStartX;
     var touchStartY;
-    var isTap;
+    var isTap = false;
+    var isFreeze = false;
     
     touch.on('touchstart',function(evt, info){
       touchStartX = info.touchStartX;
       touchStartY = info.touchStartY;
       isTap = true;
+      isFreeze = false;
     });
     touch.on('touchmove',function(evt, info){
       // var blockMoveX = (info.moveX / _this.BLOCK_SIZE) | 0;
       var moveX  = info.touchX - touchStartX;
       var moveY  = info.touchY - touchStartY;
-      var blockMoveX = (moveX / _this.BLOCK_SIZE) | 0;
+      var blockMoveX = -(moveX / _this.BLOCK_SIZE) | 0; // 左右反転
       var blockMoveY = -(moveY / _this.BLOCK_SIZE) | 0; // 上下反転
-      if (!!blockMoveX && _this.valid(blockMoveX, 0)) {
-        _this.currentX += blockMoveX;
+      
+      if (isFreeze) return;
+      
+      // 1マスずつバリデーション（すり抜け対策）
+      while (!!blockMoveX) {
+        var sign = blockMoveX / Math.abs(blockMoveX); // 1 or -1
+        if (!_this.valid(sign, 0)) break;
+        _this.currentX += sign;
+        blockMoveX -= sign;
         touchStartX = info.touchX;
       }
-      if ((blockMoveY > 0) && _this.valid(0, blockMoveY)) {
-        _this.currentY += blockMoveY;
+      while (blockMoveY > 0) {
+        if (!_this.valid(0, 1)) break;
+        _this.currentY++;
+        blockMoveY--;
         touchStartY = info.touchY;
       }
       isTap = false;
@@ -168,24 +190,45 @@
     touch.on('touchend',function(evt, info){
       if (!!isTap) _this.moveBlock('rotate');
     });
+    this.on('freeze',function(){
+      isFreeze = true;
+    });
   };
   
   // Model ------------------------------
   ns.Tetris.prototype.newGame = function() {
-    var _this = this;
+    this.initGame();
+    this.startGame();
+  };
+  
+  ns.Tetris.prototype.initGame = function() {
     clearTimeout(this.tickId);
-    this.initBoad();
-    this.nextShape();
-    this.newShape();
+    clearInterval(this.renderId);
+    this.isPlayng = false;
     this.lose = false;
     this.tickInterval = this.DEFAULT_TICK_INTERVAL;
-    this.numberOfClearLines = 0;
+    this.sumOfClearLines = 0;
     this.score = 0;
+    this.frameCount = 0;
+    this.initBoad();
+    this.initBlock();
+    this.createNextBlock();
+    this.render();
+  };
+  
+  ns.Tetris.prototype.startGame = function() {
+    var _this = this;
+    this.isPlayng = true;
+    this.createNewBlock();
+    this.createNextBlock();
     this.tick();
+    this.renderId = setInterval(function(){ _this.render(); }, this.RENDER_INTERVAL);
+    this.trigger('gamestart');
   };
   
   ns.Tetris.prototype.initBoad = function() {
-    for ( var y = 0; y < this.ROWS; ++y ) {
+    this.board = [];
+    for ( var y = 0; y < this.LOGICAL_ROWS; ++y ) {
       this.board[y] = [];
       for ( var x = 0; x < this.COLS; ++x ) {
         this.board[y][x] = 0;
@@ -193,89 +236,181 @@
     }
   };
   
-  ns.Tetris.prototype.newShape = function() {
-    this.currentBlock = this.nextBlock;
+  ns.Tetris.prototype.initBlock = function() {
+    this.currentBlock = [];
+    for ( var y = 0; y < this.NUMBER_OF_BLOCK; ++y ) {
+      this.currentBlock[y] = [];
+      for ( var x = 0; x < this.NUMBER_OF_BLOCK; ++x ) {
+        this.currentBlock[y][x] = 0;
+      }
+    }
+    this.currentBlockId = 0;
     this.currentX = this.START_X;
     this.currentY = this.START_Y;
   };
   
-  ns.Tetris.prototype.nextShape = function() {
+  ns.Tetris.prototype.createNewBlock = function() {
+    if (!this.nextBlock[0]) this.createNextBlock();
+    this.currentBlock = this.nextBlock;
+    this.currentBlockId = this.nextBlockId;
+    this.currentX = this.START_X;
+    this.currentY = this.START_Y;
+    this.trigger('newblockcreated');
+  };
+  
+  ns.Tetris.prototype.createNextBlock = function() {
     var index = Math.floor(Math.random() * this.SHAPE_LIST.length);
     var shape = this.SHAPE_LIST[index];
-    // パターンを操作ブロックへセットする
+    if (this.frameCount > 0 && this.frameCount % this.SPECIAL_SHAPE_INCIDENCE === 0) {
+      index = Math.floor(Math.random() * this.SPECIAL_SHAPE_LIST.length);
+      shape = this.SPECIAL_SHAPE_LIST[index];
+      index += this.SHAPE_LIST.length;
+    }
+    this.nextBlockId = index;
     this.nextBlock = [];
     for (var y = 0; y < this.NUMBER_OF_BLOCK; ++y) {
       this.nextBlock[y] = [];
       for (var x = 0; x < this.NUMBER_OF_BLOCK; ++x) {
         var i = this.NUMBER_OF_BLOCK * y + x;
-        if (!!shape[i]) {
-          this.nextBlock[y][x] = index + 1;
-        }
-        else {
-          this.nextBlock[y][x] = 0;
-        }
+        this.nextBlock[y][x] = (!!shape[i]) ? (index + 1) : 0;
       }
     }
+    this.trigger('nextblockcreated');
   };
   
   // メインでループする関数
-  // 操作ブロックを下の方へ動かし、操作ブロックが着地したら消去処理、ゲームオーバー判定を行う
   ns.Tetris.prototype.tick = function() {
     var _this = this;
     clearTimeout(this.tickId);
     if (!this.moveBlock('down')) {
       this.freeze();
       this.clearLines();
-      if (this.lose) {
-        this.newGame();
+      if (this.checkGameOver()) {
+        this.trigger('gameover');
+        this.quitGame().then(function(){
+          // _this.newGame();
+        });
         return false;
       }
-      this.newShape();
-      this.nextShape();
+      this.frameCount++;
+      this.createNewBlock();
+      this.createNextBlock();
     }
     this.tickId = setTimeout(function(){ _this.tick(); }, this.tickInterval);
+    this.trigger('tick');
   };
+  
+  ns.Tetris.prototype.quitGame = function() {
+    var _this = this;
+    var dfd = $.Deferred();
+    this.gameOverEffect().then(function(){
+      _this.isPlayng = false;
+      _this.trigger('gamequit');
+      dfd.resolve();
+    });
+    return dfd.promise();
+  };
+  ns.Tetris.prototype.stopGame = ns.Tetris.prototype.quitGame; // alias
   
   ns.Tetris.prototype.pauseGame = function() {
     clearTimeout(this.tickId);
   };
   
-  ns.Tetris.prototype.restartGame = function() {
+  ns.Tetris.prototype.resumeGame = function() {
     var _this = this;
+    if (!this.isPlayng) return;
     this.tickId = setTimeout(function(){ _this.tick(); }, this.tickInterval);
   };
   
-  // 操作ブロックを盤面へ固定する
   ns.Tetris.prototype.freeze = function() {
     for ( var y = 0; y < this.NUMBER_OF_BLOCK; ++y ) {
       for ( var x = 0; x < this.NUMBER_OF_BLOCK; ++x ) {
-        if (!this.currentBlock[y][x]) continue;
-        this.board[y + this.currentY][x + this.currentX] = this.currentBlock[y][x];
+        var boardX = x + this.currentX;
+        var boardY = y + this.currentY;
+        if (!this.currentBlock[y][x] || boardY < 0) continue;
+        this.board[boardY][boardX] = this.currentBlock[y][x];
       }
     }
+    this.trigger('freeze');
   };
   
   ns.Tetris.prototype.clearLines = function() {
-    var simultaneous = 0; // 同時消去ライン数
-    for ( var y = this.ROWS - 1; y >= 0; --y ) {
+    var _this = this;
+    var clearLineLength = 0; // 同時消去ライン数
+    var filledRowList = [];
+    var blankRow = Array.apply(null, Array(this.COLS)).map(function(){ return 0; }); // => [0,0,0,0,0,...]
+    var dfd = $.Deferred();
+    dfd.resolve();
+    for ( var y = this.LOGICAL_ROWS - 1; y >= 0; --y ) {
       var isRowFilled = this.board[y].every(function(val){
         return val !== 0;
       });
       if (!isRowFilled) continue;
-      simultaneous++;
-      this.numberOfClearLines++;
+      filledRowList.push(y);
+      clearLineLength++;
+      this.sumOfClearLines++;
       this.tickInterval -= this.SPEEDUP_RATE; // 1行消去で速度を上げる
       
-      // もし一行揃っていたら、ブロックを一つずつ落としていく
-      for ( var yy = y; yy > 0; --yy ) {
-        for ( var x = 0; x < this.COLS; ++x ) {
-          this.board[yy][x] = this.board[yy - 1][x];
-        }
+      // clear line effect
+      for ( var x = 0; x < this.COLS; ++x ) {
+        if (!this.board[y][x]) continue;
+        dfd = dfd
+          .then(effect(x, y))
+          .then(util.sleep(10));
       }
-      ++y;  // 一行落としたのでチェック処理を一つ戻る
     }
-    if (simultaneous <= 1) this.score += simultaneous;
-    else this.score += Math.pow(2, simultaneous);
+    // clear line drop
+    dfd.then(dropRow(x, y));
+    
+    // calc score
+    this.score += (clearLineLength <= 1) ? clearLineLength : Math.pow(2, clearLineLength);
+    
+    if (clearLineLength > 0) this.trigger("clearline", filledRowList);
+    
+    function effect(x, y) {
+      return function(){
+        var dfd = $.Deferred();
+        _this.board[y][x] = _this.CLEARLINE_BLOCK_INDEX;
+        dfd.resolve();
+        return dfd.promise();
+      };
+    }
+    function dropRow(x, y) {
+      return function(){
+        var dfd = $.Deferred();
+        if (!filledRowList.length) return;
+        filledRowList.reverse().forEach(function(row){
+          _this.board.splice(row, 1);
+          _this.board.unshift(blankRow);
+        });
+        dfd.resolve();
+        return dfd.promise();
+      };
+    }
+  };
+  
+  ns.Tetris.prototype.gameOverEffect = function() {
+    var _this = this;
+    var dfd = $.Deferred();
+    dfd.resolve();
+    for ( var y = 0; y < this.LOGICAL_ROWS; ++y ) {
+      for ( var x = 0; x < this.COLS; ++x ) {
+        if (!this.board[y][x]) continue;
+        // this.board[y][x] = this.BLOCK_IMAGE_LIST.length - 1;
+        dfd = dfd
+          .then(effect(x, y))
+          .then(util.sleep(10));
+      }
+    }
+    function effect(x, y) {
+      return function(){
+        var dfd = $.Deferred();
+        _this.board[y][x] = _this.GAMEOVER_BLOCK_INDEX;
+        dfd.resolve();
+        return dfd.promise();
+      };
+    }
+    return dfd.then(util.sleep(500)).promise();
   };
   
   ns.Tetris.prototype.moveBlock = function(code) {
@@ -323,30 +458,25 @@
     return newBlock;
   };
   
-  // 指定された方向に、操作ブロックを動かせるかどうかチェックする ＆ ゲームオーバー判定もここで行う
   ns.Tetris.prototype.valid = function(offsetX, offsetY, newBlock) {
     offsetX = offsetX || 0;
     offsetY = offsetY || 0;
     var nextX = this.currentX + offsetX;
     var nextY = this.currentY + offsetY;
-    newBlock = newBlock || this.currentBlock;
-    // console.log(offsetX, offsetY, nextX, nextY);
+    block = newBlock || this.currentBlock;
     
     for ( var y = 0; y < this.NUMBER_OF_BLOCK; ++y ) {
       for ( var x = 0; x < this.NUMBER_OF_BLOCK; ++x ) {
-        if (!newBlock[y][x]) continue;
-        // if ((y + nextY) < 0) continue;
-        if ( typeof this.board[ y + nextY ] === 'undefined' // 次の位置が盤面外なら
-          || typeof this.board[ y + nextY ][ x + nextX ] === 'undefined' // 盤面外なら
-          || this.board[ y + nextY ][ x + nextX ] // 次の位置にブロックがあれば
-          || x + nextX < 0 // 盤面外なら
-          || y + nextY >= this.ROWS // 盤面外なら
-          || x + nextX >= this.COLS ) { // 盤面外なら
-          // if (nextY === 1) lose = true; // lose if the current shape at the top row when checked
-          if (nextY === 1 && offsetY === 1) { // 次のy座標が1ならゲームオーバー
-            console.log('game over');
-            this.lose = true;
-          }
+        var boardX = x + nextX;
+        var boardY = y + nextY;
+        if (!block[y][x]) continue;
+        if ( typeof this.board[boardY] === 'undefined' // 次の位置が盤面外なら
+          || typeof this.board[boardY][boardX] === 'undefined' // 盤面外なら
+          || this.board[boardY][boardX] // 次の位置にブロックがあれば
+          || boardX < 0 // 左壁
+          || boardX >= this.COLS // 右壁
+          || boardY >= this.LOGICAL_ROWS ) { // 底面
+          
           return false;
         }
       }
@@ -355,37 +485,28 @@
   };
   
   ns.Tetris.prototype.checkGameOver = function() {
+    // ブロックの全てが画面外ならゲームオーバー
+    var isGameOver = true;
+    for ( var y = 0; y < this.NUMBER_OF_BLOCK; ++y ) {
+      for ( var x = 0; x < this.NUMBER_OF_BLOCK; ++x ) {
+        var boardX = x + this.currentX;
+        var boardY = y + this.currentY;
+        if (boardY >= this.HIDDEN_ROWS) {
+          isGameOver = false;
+          break;
+        }
+      }
+    }
+    return isGameOver;
   };
   
   // View ------------------------------
   ns.Tetris.prototype.render = function() {
     this.ctx.clearRect(0, 0, this.WIDTH, this.HEIGHT);
-    // 盤面を描画する
-    for (var x = 0; x < this.COLS; ++x) {
-      for (var y = 0; y < this.ROWS; ++y) {
-        if (!this.board[y][x]) continue;
-        this.ctx.fillStyle = this.COLOR_LIST[this.board[y][x] - 1];
-        this.drawBlock(x, y);
-      }
-    }
-    // 操作ブロックを描画する
-    for (var y = 0; y < this.NUMBER_OF_BLOCK; ++y) {
-      for (var x = 0; x < this.NUMBER_OF_BLOCK; ++x) {
-        if (!this.currentBlock[y][x]) continue;
-        this.ctx.fillStyle = this.COLOR_LIST[this.currentBlock[y][x] - 1];
-        this.drawBlock(this.currentX + x, this.currentY + y);
-      }
-    }
     
-    // Nextブロック描画
-    this.ctxNext.clearRect(0, 0, this.NEXT_WIDTH, this.NEXT_HEIGHT);
-    for (var y = 0; y < this.NUMBER_OF_BLOCK; ++y) {
-      for (var x = 0; x < this.NUMBER_OF_BLOCK; ++x) {
-        if (!this.nextBlock[y][x]) continue;
-        this.ctxNext.fillStyle = this.COLOR_LIST[this.nextBlock[y][x] - 1];
-        this.drawNextBlock(x, y);
-      }
-    }
+    this.renderBoard();
+    this.renderCurrentBlock();
+    this.renderNextBlock();
     
     // clear lines表示
     this.$clearLine.text(this.numberOfClearLines);
@@ -394,18 +515,58 @@
     this.$score.text(this.score);
   };
   
-  ns.Tetris.prototype.drawBlock = function(x, y) {
+  ns.Tetris.prototype.renderBoard = function() {
+    // 盤面を描画する
+    for (var x = 0; x < this.COLS; ++x) {
+      for (var y = 0; y < this.ROWS; ++y) {
+        var boardX = x;
+        var boardY = y + this.HIDDEN_ROWS;
+        var blockId = this.board[boardY][boardX] - 1;
+        if (!this.board[boardY][boardX] || blockId < 0) continue;
+        this.drawBlock(x, y, blockId);
+      }
+    }
+  };
+  
+  ns.Tetris.prototype.renderCurrentBlock = function() {
+    // 操作ブロックを描画する
+    for (var y = 0; y < this.NUMBER_OF_BLOCK; ++y) {
+      for (var x = 0; x < this.NUMBER_OF_BLOCK; ++x) {
+        var blockId = this.currentBlock[y][x] - 1;
+        if (!this.currentBlock[y][x] || blockId < 0) continue;
+        var drawX = x + this.currentX;
+        var drawY = y + this.currentY - this.HIDDEN_ROWS;
+        this.drawBlock(drawX, drawY, blockId);
+      }
+    }
+  };
+  
+  ns.Tetris.prototype.renderNextBlock = function() {
+    // Nextブロック描画
+    this.ctxNext.clearRect(0, 0, this.NEXT_WIDTH, this.NEXT_HEIGHT);
+    for (var y = 0; y < this.NUMBER_OF_BLOCK; ++y) {
+      for (var x = 0; x < this.NUMBER_OF_BLOCK; ++x) {
+        var blockId = this.nextBlock[y][x] - 1;
+        if (!this.nextBlock[y][x] || blockId < 0) continue;
+        this.drawNextBlock(x, y, blockId);
+      }
+    }
+  };
+  
+  ns.Tetris.prototype.drawBlock = function(x, y, id) {
     var blockX = this.BLOCK_SIZE * x;
     var blockY = this.BLOCK_SIZE * y;
-    var blockSize = this.BLOCK_SIZE - 1; // border分-1？
+    var blockSize = this.BLOCK_SIZE;
+    this.ctx.fillStyle = this.COLOR_LIST[id];
     this.ctx.fillRect( blockX, blockY, blockSize, blockSize );
     this.ctx.strokeRect( blockX, blockY, blockSize, blockSize );
   };
   
-  ns.Tetris.prototype.drawNextBlock = function(x, y) {
+  ns.Tetris.prototype.drawNextBlock = function(x, y, id) {
     var blockX = this.BLOCK_SIZE * x;
     var blockY = this.BLOCK_SIZE * y;
-    var blockSize = this.BLOCK_SIZE - 1; // border分-1？
+    var blockSize = this.BLOCK_SIZE;
+    this.ctxNext.fillStyle = this.COLOR_LIST[id];
     this.ctxNext.fillRect( blockX, blockY, blockSize, blockSize );
     this.ctxNext.strokeRect( blockX, blockY, blockSize, blockSize );
   };
--- src/js/main.js
+++ src/js/main.js
@@ -3,36 +3,32 @@
   
   var $win = $(win);
   var util = new ns.Util();
-  // util.bindOnResize();
   
-  $(function(){
-    
-    if (ns.ua.isSP) {
-      // sp
-      $(".onlypc").remove();
-    }
-    else {
-      // pc
-      $(".onlysp").remove();
-    }
-    
-    var tetris = new ns.Tetris();
-    
-    var query = util.getQueryString();
-    
-    if (query.debug === 'true') {
-      var line = query.line || 5;
-      var newary = tetris.SHAPE_LIST.map(function(){
-        var ary = [];
-        for (var i = 0; i < line; ++i) {
-          ary = ary.concat([1, 1, 1, 1, 1]);
-        }
-        return ary;
-      });
-      tetris.SHAPE_LIST = newary;
-      tetris.newGame();
-    }
-    
-  });
+  if (ns.ua.isSP) {
+    // sp
+    $(".onlypc").remove();
+  }
+  else {
+    // pc
+    $(".onlysp").remove();
+  }
+  
+  var tetris = new ns.Tetris();
+  tetris.newGame();
+  
+  // debug
+  var query = util.getQueryString();
+  if (query.debug) {
+    var line = +query.debug || 5;
+    var newary = tetris.SHAPE_LIST.map(function(){
+      var ary = [];
+      for (var i = 0; i < line; ++i) {
+        ary = ary.concat([1, 1, 1, 1, 1]);
+      }
+      return ary;
+    });
+    tetris.SHAPE_LIST = newary;
+    tetris.newGame();
+  }
   
 })(this, document);
