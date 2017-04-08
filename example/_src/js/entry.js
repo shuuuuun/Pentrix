68f139c03f57e1182e88c11e82272a0bcd16b5c3
motoki-shun <motoki-shun@kayac.com>
2017-04-08 22:31:20 +0900

    example: block image with image query

--- example/_src/js/entry.js
+++ example/_src/js/entry.js
@@ -1,15 +1,21 @@
+import qs from 'querystring';
+
+// Location
+const locationHash = (location.hash || '').replace(/^#/, '');
+const locationSearch = (location.search || '').replace(/^\?/, '');
+const locationParams = qs.parse(locationSearch);
+
 // Init
 const sample_image_list = ['./img/block_1.png','./img/block_2.png','./img/block_3.png','./img/block_4.png','./img/block_5.png','./img/block_6.png','./img/block_7.png','./img/block_8.png','./img/block_9.png'];
 const container = document.querySelector('.container');
 const gameRoot = document.getElementById('game-root');
 const pentrix = new Pentrix({
   rootElement: gameRoot,
-  // useDefaultImage: false,
-  // blockImageList: sample_image_list.map(src => {
-  //   const img = new Image();
-  //   img.src = src;
-  //   return img;
-  // }),
+  blockImageList: locationParams.image === undefined ? null : sample_image_list.map(src => {
+    const img = new Image();
+    img.src = src;
+    return img;
+  }),
 });
 
 
